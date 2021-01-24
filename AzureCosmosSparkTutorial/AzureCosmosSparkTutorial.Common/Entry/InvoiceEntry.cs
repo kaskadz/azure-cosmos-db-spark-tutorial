7971e55c5ba8f99fb7369704d6ca8271b5dcea82
Kasper <kasper.kadzielawa@gmail.com>
2021-01-24 10:27:45 +0100

    Remove redundant base class

--- AzureCosmosSparkTutorial/AzureCosmosSparkTutorial.Common/Entry/InvoiceEntry.cs
+++ AzureCosmosSparkTutorial/AzureCosmosSparkTutorial.Common/Entry/InvoiceEntry.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
 
 namespace AzureCosmosSparkTutorial.Common.Entry
 {
-    public class TransactionElementEntry : EntryBase, IPartitionKeyModel
+    public class TransactionElementEntry : IPartitionKeyModel
     {
         public static readonly string PartitionKeyPath = $"/{nameof(Country)}";
 
+        [JsonProperty(PropertyName = "id")] public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime InvoiceDate { get; set; }
         public string Country { get; set; }
         public long? CustomerId { get; set; }
-        
+
         public IEnumerable<ItemEntry> Items { get; set; }
 
         public PartitionKey GetPartitionKey() => new(Country);
